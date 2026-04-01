b0d23ecc90677edfa19eaab9e4ad4af82ff944e8
Lakony34 <nicolas.demars@epfedu.fr>
2026-04-01 16:24:07 +0200

    [Add] Initial ePortfolio setup
    - Created FastAPI backend in main.py to handle application logic
    - Initialized FastAPI app instance
    - Implemented basic routing (home and admin pages)
    - Added in-memory storage for projects using a Python list
    - Integrated Jinja2 templating for dynamic HTML rendering
    - Implemented form handling to create new projects via POST request
    - Added redirect after project creation for better user flow

--- main.py
+++ main.py
@@ -1,25 +1,74 @@
-from typing import Annotated
-
 from fastapi import FastAPI, Request, Form, status, HTTPException
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
-import pandas as pd
 from pydantic import BaseModel
 
 app = FastAPI()
-file = pd.read_csv("data.csv")
 
 
-class Identity(BaseModel):
-    name : str
-    surname : str
-    age : int
+class Project(BaseModel):
+    name: str
+    description: str
+    is_published: bool | None = None
 
-@app.get("/data")
-def get_data():
-    return file.to_dict(orient="records")
 
-@app.get("/identity")
-def read_identity():
-    identity = Identity(name="John", surname="Doe", age=30)
-    return {identity}
+projects: list[Project]
+projects = []
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+def show_home(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "index.html",
+        context={"projects": projects, "welcome_message": "Bienvenue sur mon ePortfolio !"},
+    )
+
+
+@app.get("/admin", response_class=HTMLResponse)
+def show_admin(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "admin.html",
+        context={"projects": projects},
+    )
+
+
+@app.get("/projects/{project_id}")
+def read_project(project_id: int, detail: int | None = None):
+    return {"project_id": project_id, "project": projects[project_id], "detail": detail}
+
+
+@app.post("/projects")
+def create_project(
+    name: Annotated[str, Form()],
+    description: Annotated[str, Form()],
+    is_published: Annotated[str, Form()],
+    admin: Annotated[str, Form()] = None,
+):
+    if admin != "true":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Not admin action",
+        )
+
+    project = {
+        "name": name,
+        "description": description,
+        "is_published": True if is_published == "true" else False,
+    }
+
+    projects.append(project)
+
+    return RedirectResponse("/admin", status_code=303)
+
+
+@app.put("/projects/{project_id}")
+def update_project(project_id: int, project: Project):
+    projects[project_id].update(project.dict())
+
+    response = {"project_name": projects[project_id]["name"], "project_id": project_id}
+
+    return response
